7c7c03ba68627501659e6d6c3dd51400dbbb1c7b
SamAng219255 <SamAng033146085@gmail.com>
2026-02-04 01:10:57 -0800

    Actually connected the product endpoints.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 //router.use("/user/", require("./user"));
 
-//router.use("/product/", require("./product"));
+router.use("/product/", require("./product"));
 
 //router.use("/order/", require("./order"));
 
