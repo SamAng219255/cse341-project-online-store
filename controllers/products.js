1c6e030ebbbcc4737c6c226d5524a24a871be92f
SamAng219255 <SamAng033146085@gmail.com>
2026-02-04 01:06:28 -0800

    Added products api endpoints.

---
+++ controllers/products.js
@@ -0,0 +1,231 @@
+const model = require("../models/example");
+const { DBNotReadyError, InvalidDataError, NotFoundError, ConflictingValueError } = require("../error_types");
+
+const getProductById = async(req, res, next) => {
+	let data;
+	try {
+		if(req.params.productId == undefined)
+			throw new InvalidDataError();
+
+		data = await productsModel.getProductById(req.params.productId);
+	}
+	/*
+		#swagger.responses[200] = {
+			description: 'Product found and returned.',
+			schema: {
+				id: "000000000000000000000000",
+				name: "Sweatshirt",
+				description: "A classic everyday sweatshirt made from a soft, mid-weight cotton blend for all-day comfort. Designed with a relaxed fit, ribbed cuffs, and a durable finish that holds up to regular wear and washing. Perfect for layering or wearing on its own in any season.",
+				price: 1999,
+				inStock: "in",
+				discount: 10,
+				options: [["red", "blue", "gray", "black"], ["XS", "S", "M", "L", "XL", "XXL", "XXXL"]]
+			}
+		}
+		#swagger.responses[503] = { description: 'Server still turning on and not yet connected to database.' }
+		#swagger.responses[400] = { description: 'Client provided an invalid product id.' }
+		#swagger.responses[404] = { description: 'No product with the provided id exists.' }
+	*/
+	catch(err) {
+		if(err instanceof DBNotReadyError)
+			res.status(503).json({ message: "Database not yet ready." });
+		else if(err instanceof InvalidDataError)
+			res.status(400).json({ message: "Data is missing or invalid." });
+		else if(err instanceof NotFoundError)
+			res.status(404).json({ message: "Requested product not found" });
+		else {
+			console.error(`getProductById: ${err.name}: ${err.message}`);
+			res.sendStatus(500);
+		}
+	}
+	res.status(200).json(data);
+};
+
+const getAllProducts = async(req, res, next) => {
+	let data;
+	try {
+		data = await productsModel.getAllProducts();
+	}
+	/*
+		#swagger.responses[200] = {
+			description: 'Returns a list of all products.',
+			schema: [{
+				id: "000000000000000000000000",
+				name: "Sweatshirt",
+				description: "A classic everyday sweatshirt made from a soft, mid-weight cotton blend for all-day comfort. Designed with a relaxed fit, ribbed cuffs, and a durable finish that holds up to regular wear and washing. Perfect for layering or wearing on its own in any season.",
+				price: 1999,
+				inStock: "in",
+				discount: 10,
+				options: [["red", "blue", "gray", "black"], ["XS", "S", "M", "L", "XL", "XXL", "XXXL"]]
+			}]
+		}
+		#swagger.responses[503] = { description: 'Server still turning on and not yet connected to database.' }
+	*/
+	catch(err) {
+		if(err instanceof DBNotReadyError)
+			res.status(503).json({ message: "Database is not yet ready." });
+		else {
+			console.error(`getAllProducts: ${err.name}: ${err.message}`);
+			res.sendStatus(500);
+		}
+	}
+	res.status(200).json(data);
+};
+
+const createProduct = async(req, res, next) => {
+	/*
+	#swagger.parameters['New Product Information'] = {
+		in: 'body',
+		description: 'New value to update product record with.',
+		required: true,
+		schema: {
+			$name: "Sweatshirt",
+			$description: "A classic everyday sweatshirt made from a soft, mid-weight cotton blend for all-day comfort. Designed with a relaxed fit, ribbed cuffs, and a durable finish that holds up to regular wear and washing. Perfect for layering or wearing on its own in any season.",
+			$price: 1999,
+			$inStock: "in",
+			$discount: 10,
+			$options: [["red", "blue", "gray", "black"], ["XS", "S", "M", "L", "XL", "XXL", "XXXL"]]
+		}
+	}
+	*/
+	let id;
+	try {
+		if(req.body.githubId && await productsModel.githubExists(req.body.githubId, req.params.id))
+			throw new ConflictingValueError();
+
+		id = await productsModel.createProduct(req.body);
+	}
+	/*
+		#swagger.responses[201] = {
+			description: 'Product successfully added.',
+			schema: {
+				id: "000000000000000000000000"
+			}
+		}
+		#swagger.responses[503] = { description: 'Server still turning on and not yet connected to database.' }
+		#swagger.responses[400] = { description: 'Client did not provide sufficient data to create a product record or provided invalid data.' }
+		#swagger.responses[401] = { description: 'Attempted to access the endpoint without authenticating.' }
+		#swagger.responses[403] = { description: 'Attempted to access the endpoint without matching authorization.' }
+	*/
+	catch(err) {
+		if(err instanceof DBNotReadyError)
+			res.status(503).json({ message: "Database not yet ready." });
+		else if(err instanceof InvalidDataError)
+			res.status(400).json({ message: "Data is missing or invalid." });
+		else if(err.name == "ValidationError")
+			res.status(400).json(err);
+		else {
+			console.error(`createProduct: ${err.name}: ${err.message}`);
+			res.sendStatus(500);
+		}
+	}
+	res.status(201).json({ id });
+};
+
+const updateProduct = async(req, res, next) => {
+	/*
+	#swagger.parameters['Updated Product Information'] = {
+		in: 'body',
+		description: 'New value to update product record with.',
+		required: true,
+		schema: {
+			$name: "Sweatshirt",
+			$description: "A classic everyday sweatshirt made from a soft, mid-weight cotton blend for all-day comfort. Designed with a relaxed fit, ribbed cuffs, and a durable finish that holds up to regular wear and washing. Perfect for layering or wearing on its own in any season.",
+			$price: 1999,
+			$inStock: "in",
+			$discount: 10,
+			$options: [["red", "blue", "gray", "black"], ["XS", "S", "M", "L", "XL", "XXL", "XXXL"]]
+		}
+	}
+	*/
+	let data;
+	try {
+		if(req.params.productId == undefined)
+			throw new InvalidDataError();
+		if(req.body.githubId && await productsModel.githubExists(req.body.githubId, req.params.productId))
+			throw new ConflictingValueError();
+
+		data = await productsModel.updateProduct(req.params.productId, req.body);
+	}
+	/*
+		#swagger.responses[200] = {
+			description: 'Product found and updated.',
+			schema: {
+				id: "000000000000000000000000",
+				name: "Sweatshirt",
+				description: "A classic everyday sweatshirt made from a soft, mid-weight cotton blend for all-day comfort. Designed with a relaxed fit, ribbed cuffs, and a durable finish that holds up to regular wear and washing. Perfect for layering or wearing on its own in any season.",
+				price: 1999,
+				inStock: "in",
+				discount: 10,
+				options: [["red", "blue", "gray", "black"], ["XS", "S", "M", "L", "XL", "XXL", "XXXL"]]
+			}
+		}
+		#swagger.responses[503] = { description: 'Server still turning on and not yet connected to database.' }
+		#swagger.responses[400] = { description: 'Client provided an invalid product id or body.' }
+		#swagger.responses[404] = { description: 'No product with the provided id exists.' }
+		#swagger.responses[401] = { description: 'Attempted to access the endpoint without authenticating.' }
+		#swagger.responses[403] = { description: 'Attempted to access the endpoint without matching authorization.' }
+	*/
+	catch(err) {
+		if(err instanceof DBNotReadyError)
+			res.status(503).json({ message: "Database not yet ready." });
+		else if(err instanceof InvalidDataError)
+			res.status(400).json({ message: "Data is missing or invalid." });
+		else if(err instanceof NotFoundError)
+			res.status(404).json({ message: "Requested product not found" });
+		else if(err.name == "ValidationError")
+			res.status(400).json(err);
+		else {
+			console.error(`updateProduct: ${err.name}: ${err.message}`);
+			res.sendStatus(500);
+		}
+	}
+	res.status(200).json(data);
+};
+
+const deleteProduct = async(req, res, next) => {
+	try {
+		if(req.params.productId == undefined)
+			throw new InvalidDataError();
+
+		await tasksModel.removeTasksByProduct(req.params.productId);
+		await productsModel.deleteProduct(req.params.productId);
+	}
+	/*
+		#swagger.responses[204] = { description: 'Product found and deleted.' }
+		#swagger.responses[503] = { description: 'Server still turning on and not yet connected to database.' }
+		#swagger.responses[400] = { description: 'Client provided an invalid product id.' }
+		#swagger.responses[404] = { description: 'No product with the provided id exists.' }
+		#swagger.responses[401] = { description: 'Attempted to access the endpoint without authenticating.' }
+		#swagger.responses[403] = { description: 'Attempted to access the endpoint without matching authorization.' }
+	*/
+	catch(err) {
+		if(err instanceof DBNotReadyError)
+			res.status(503).json({ message: "Database not yet ready." });
+		else if(err instanceof InvalidDataError)
+			res.status(400).json({ message: "Data is missing or invalid." });
+		else if(err instanceof NotFoundError)
+			res.status(404).json({ message: "Requested product not found" });
+		else {
+			console.error(`deleteProduct: ${err.name}: ${err.message}`);
+			res.sendStatus(500);
+		}
+	}
+	try {
+		req.logout(function(err) {
+			if(err) return next(err);
+		});
+	}
+	finally {
+		req.session.destroy();
+	}
+	res.sendStatus(204);
+};
+
+module.exports = {
+	getAllProducts,
+	getProductById,
+	createProduct,
+	updateProduct,
+	deleteProduct,
+};
---
+++ models/products.js
@@ -0,0 +1,154 @@
+const { mongoose, wrapReadyCheck } = require("../database/");
+const { InvalidDataError, NotFoundError } = require("../error_types");
+
+const ObjectId = mongoose.Types.ObjectId;
+
+const _model = mongoose.model(
+	"products",
+	mongoose.Schema(
+		{
+			name: String,
+			description: String,
+			price: {
+				type: Number,
+				validate: Number.isInteger,
+			},
+			inStock: {
+				type: String,
+				enum: [
+					"in",
+					"<100",
+					"<10",
+					"5",
+					"4",
+					"3",
+					"2",
+					"1",
+					"out"
+				]
+			},
+			discount: {
+				type: Number,
+				validate: Number.isInteger,
+			},
+			options: [[String]],
+
+		},
+		{ timestamps: true },
+	),
+);
+
+const keys = [
+	"name",
+	"description",
+	"price",
+	"inStock",
+	"discount",
+	"options",
+];
+
+const copyNeededKeys = oldObj => Object.fromEntries(keys.filter(key => key in oldObj).map(key => [ key, oldObj[key] ]));
+
+const getAllProducts = wrapReadyCheck(async id => {
+	try {
+		return await _model.find();
+	}
+	catch(err) {
+		console.error(`getAllProducts/find: ${err.name}: ${err.message}`);
+		throw err;
+	}
+});
+
+const getProductById = wrapReadyCheck(async id => {
+	let _id;
+	try {
+		_id = new ObjectId(id);
+	}
+	catch(err) {
+		console.error(`getProductById/ObjectId: ${err.name}: ${err.message}`);
+		throw new InvalidDataError();
+	}
+
+	let result;
+	try {
+		result = await _model.findById(_id);
+	}
+	catch(err) {
+		console.error(`getProductById/findById: ${err.name}: ${err.message}`);
+		throw err;
+	}
+
+	if(result == null)
+		throw new NotFoundError();
+
+	return result;
+});
+
+const createProduct = wrapReadyCheck(async data => {
+	if(!keys.every(key => key in data))
+		throw new InvalidDataError();
+
+	const _id = new ObjectId();
+
+	try {
+		await _model.create({ _id, ...copyNeededKeys(data) }); // Data is automatically validated against the schema
+	}
+	catch(err) {
+		console.error(`createProduct/create: ${err.name}: ${err.message}`);
+		throw err;
+	}
+
+	return _id.toString();
+});
+
+const updateProduct = wrapReadyCheck(async(id, data) => {
+	let _id;
+	try {
+		_id = new ObjectId(id);
+	}
+	catch(err) {
+		console.error(`updateProduct/ObjectId: ${err.name}: ${err.message}`);
+		throw new InvalidDataError();
+	}
+
+	if(await _model.findById(_id) == null)
+		throw new NotFoundError();
+
+	const cleanedData = copyNeededKeys(data);
+
+	try {
+		await new mongoose.Document(cleanedData, _model.schema).validate();
+
+		await _model.updateOne({ _id }, cleanedData);
+	}
+	catch(err) {
+		console.error(`updateProduct/updateOne: ${err.name}: ${err.message}`);
+		throw err;
+	}
+
+	return await _model.findById(_id);
+});
+
+const deleteProduct = wrapReadyCheck(async id => {
+	let _id;
+	try {
+		_id = new ObjectId(id);
+	}
+	catch(err) {
+		console.error(`deleteProduct/ObjectId: ${err.name}: ${err.message}`);
+		throw new InvalidDataError();
+	}
+
+	const result = await _model.deleteOne({ _id });
+
+	if(result.deletedCount < 1)
+		throw new NotFoundError();
+});
+
+module.exports = {
+	getAllProducts,
+	getProductById,
+	createProduct,
+	updateProduct,
+	deleteProduct,
+};
