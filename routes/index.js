e706d36b8083b2b9518b1eb817aa987e9684d9cf
Danae-DJ <danae_taiyaki@hotmail.com>
2026-02-04 18:19:27 -0600

    order and user

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-//router.use("/user/", require("./user"));
+router.use("/user/", require("./user"));
 
 router.use("/product/", require("./products"));
 
-//router.use("/order/", require("./order"));
+router.use("/order/", require("./order"));
 
 //router.use("/review/", require("./review"));
 
